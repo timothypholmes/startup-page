9c0e400fe5e23c32a7dbb9204a9cfc7004763faa
Tim Holmes <tpholmes7@gmail.com>
2022-02-02 16:32:15 -0600

    start adding components (unsplach/search/clock

--- src/views/Index.js
+++ src/views/Index.js
@@ -6,26 +6,55 @@ import { Link } from "react-router-dom";
 //import Footer from "components/Footers/Footer.js";
 import Clock from "../components/Clock.js";
 import Unsplash from "../components/Unsplash.js";
+import SearchBox from "../components/Search.js";
+import SolarGraph from "../components/SolarGraph.js";
 
 
 export default function Index() {
   return (
     <>
       <section className="bg-[#0d1a26] items-center flex h-screen max-h-860-px p-20">
-        <div class="grid grid-rows-3 gap-8">
-          <div class="bg-[#0d1a26] text-white rounded-md col-start-1 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">
-            <video src="./assets/img/chicago3.mp4" width="600" height="600" autoplay="true" />
-          </div>
+        <div class="grid grid-rows-3 gap-11">
+
+          {/* row 1 */}
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-1 col-span-1 row-span-2 shrink-0 h-92 w-36 border-dashed border-2 border-[#00000]">01</div>
           <div class="bg-[#0d1a26] text-white rounded-md col-start-2 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">02</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-3 col-span-2 shrink-0 h-36 w-92 border-dashed border-2 border-[#00000]"><SearchBox /></div>
           <div class="bg-[#0d1a26] text-white rounded-md col-start-5 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">05</div>
-          <div class="bg-[#0d1a26] text-white rounded-md col-span-1 shrink-0 h-36 w-36">
-            <Unsplash />
-          </div>
-          <div class="bg-[#0d1a26] text-white rounded-md col-span-1 shrink-0 h-36 w-36">
-            <Clock />
-          </div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-6 col-span-1 shrink-0 h-36 w-36"><Unsplash /></div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-7 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">07</div>
+         
+          {/* row 2 */}
+          
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-2 col-span-1 shrink-0 h-36 w-36"><Unsplash /></div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-3 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">03</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-4 col-span-1 shrink-0 h-36 w-36"><Unsplash /></div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-5 col-span-2 row-span-2 shrink-0 h-92 w-92 border-dashed border-2 border-[#00000]">05</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-7 col-span-1 shrink-0 h-36 w-36"><Unsplash /></div>
+
+          {/* row 3 */}
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-1 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">01</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-2 col-span-2 row-span-2 shrink-0 h-92 w-92 border-dashed border-2 border-[#00000]">02</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-4 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">04</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-7 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">07</div>
+
+          {/* row 4 */}
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-1 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">01</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-4 col-span-1 shrink-0 h-36 w-36"><Unsplash /></div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-5 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">05</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-6 col-span-1 shrink-0 h-36 w-36"><Unsplash /></div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-7 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]"><Clock /></div>
+
+          {/* row 5 */}
+          {/*
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-1 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]"></div>
           <div class="bg-[#0d1a26] text-white rounded-md col-start-2 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">02</div>
-          <div class="bg-[#0d1a26] text-white rounded-md col-start-2 col-span-1 shrink-0 h-36 w-80 border-dashed border-2 border-[#00000]">04</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-3 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">05</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-4 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]"></div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-5 col-span-2 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">02</div>
+          <div class="bg-[#0d1a26] text-white rounded-md col-start-7 col-span-1 shrink-0 h-36 w-36 border-dashed border-2 border-[#00000]">05</div>
+          */}
+
         </div>
       </section>
     </>
