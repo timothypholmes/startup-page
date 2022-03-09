335daff9213abf83e68e2beafa4b2ca1dc84cc2a
Tim Holmes <tpholmes7@gmail.com>
2022-03-08 20:25:12 -0600

    fix broke twitter component and disconnet

--- src/components/Twitter.jsx
+++ src/components/Twitter.jsx
@@ -1,45 +1,29 @@
 import React from "react";
+import axios from 'axios';
+
 
 class Twitter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tweet: this.props.tweet,
-      applications: null
-    }
-  }
-
-  fetchData = () => {
-    var user = "DeItaone"
-    var maxResult = 1
-    var url = `https://api.twitter.com/2/tweets/search/recent?query=from:${user}&max_results=${maxResult}`
-
-    
-    fetch('http://localhost:3001/api/tweet')
-      .then((response) => response.json())
-      .then(data => {
-        console.log(data)
-    })
-    
-  
-    fetch('http://localhost:3001/api/tweet')
-    .then(response => response.json())
-    .then(data => this.setState({ applications: data }));
-      //.then(body => {console.log(body)})
-
-    console.log(this.state.applications)
+      posts: []
+    };
   }
 
   //async componentDidMount() {
   componentDidMount() {
-    this.fetchData()
+    fetch('http://localhost:3001/api/tweet')
+      .then((response) => response.json())
+      .then(data => {
+        console.log(data)
+      })
   }
 
 	render() {
     return (
       <>
       <div class="text-center items-center justify-center translate-x-0 translate-y-0">
-        <h1 class="text-3xl pt-5 text-off-white1">{this.state.tweet}</h1>
+        <h1 class="text-3xl pt-5 text-off-white1">{this.state.posts}</h1>
       </div>
       </>
     );
