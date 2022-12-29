1ff5dd2966fcd05e934ac6a39fe22c902ce7411d
Tim Holmes <tpholmes7@gmail.com>
2022-12-29 11:49:37 -0600

    add in config file for bookmarks/fix other issues

---
+++ src/config/index.example.js
@@ -0,0 +1,94 @@
+var config = {
+
+    bookmark: [
+        {
+            title: 'news',
+            content: [
+                {
+                    name: "hacker news",
+                    url: "https://news.ycombinator.com/"
+                },
+                {
+                    name: "medium",
+                    url: "https://medium.com"
+                },
+                {
+                    name: "12ft",
+                    url: "https://12ft.io"
+                }
+            ]
+        },
+        {
+            title: 'work',
+            content: [
+                {
+                    name: "stackoverflow",
+                    url: "https://stackoverflow.com"
+                },
+                {
+                    name: "github",
+                    url: "https://github.com"
+                }
+            ]
+        },
+        {
+            title: 'finance',
+            content: [
+                {
+                    name: "tdameritrade",
+                    url: "https://secure.tdameritrade.com/"
+                },
+                {
+                    name: "options",
+                    url: "https://secure.tdameritrade.com/trade/options"
+                },
+                {
+                    name: "stocks/etfs",
+                    url: "https://secure.tdameritrade.com/trade"
+                },
+                {
+                    name: "marketwatch",
+                    url: "https://www.marketwatch.com"
+                },
+            ]
+        },
+        {
+            title: 'social',
+            content: [
+                {
+                    name: "reddit",
+                    url: "https://www.reddit.com"
+                },
+                {
+                    name: "youtube",
+                    url: "https://www.youtube.com"
+                },
+                {
+                    name: "linkedin",
+                    url: "https://www.linkedin.com"
+                },
+            ]
+        },
+        {
+            title: 'home lab',
+            content: [
+                {
+                    name: "homebridge",
+                    url: "http://10.0.0.182:8581/login"
+                },
+                {
+                    name: "pi-hole",
+                    url: "http://10.0.0.182/admin/index.php"
+                },
+            ]
+        },
+    ],
+  
+    style: {
+        background: 'bg-[#F2F2F2]',
+        navBackground: 'bg-[#F2F2F2]',
+        underline: 'decoration-blue-500'
+    }
+}
+
+export default config
